8286cd51bc02ce057c7a029ef198480b540dd91d
rentoo <dreamyiz@outlook.com>
2020-12-16 14:55:48 +0800

    -

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+// console.log('object');
+// div = document.createElement('div')
+// div.textContent = 'Hel lo'
+// document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,82 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/main.js",
   watch: true,
   watchOptions: {
-    ignored: ['files/**/*.js', 'node_modules/**'],
+    ignored: ["files/**/*.js", "node_modules/**"],
     aggregateTimeout: 200,
-    poll: 1000
-  } ,
+    poll: 1000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "/",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "Caching",
+    }),
+    new CopyPlugin({
+      patterns: [path.resolve(__dirname, "public"), path.resolve(__dirname, "static")],
+    }),
+  ],
   devServer: {
     before: function (app, server, compiler) {
       app.get("/some/path", function (req, res) {
         res.json({ custom: "response" });
       });
     },
+    // lazy: true,
+    hotOnly: true,
     contentBase: path.join(__dirname, "dist"),
-    compress: true,
+    // liveReload: false,
+    host: "0.0.0.0",
+    open: "Google Chrome",
     port: 9000,
     allowedHosts: ["host.com", "subdomain.host.com"],
+    injectHot: (compilerConfig) => compilerConfig.name === "only-include",
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
 };
